85d184565b31e08f91f7630dd431e01e27dd1920
GilbertXiao <gilxyj@163.com>
2020-03-04 21:13:03 +0800

    spring cloud Resilience4j

---
+++ resilience4j-2/src/main/java/com/gilxyj/resilience4j2/Resilience4j2Application.java
@@ -0,0 +1,13 @@
+package com.gilxyj.resilience4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4j2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4j2Application.class, args);
+    }
+
+}
